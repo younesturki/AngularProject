b6acd8968f2cd0b5d1c665227d783e629c48a10a
younesturki <younesturki@live.fr>
2020-03-15 11:40:10 +0100

    Phase 1

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe','This is simply a test',
+ 'https://www.supermarchesmatch.fr/themes/smatch/img/boite-recettes.png')];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
